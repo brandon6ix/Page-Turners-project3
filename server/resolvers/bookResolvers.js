712c6c3e655ecb3969886620dee276d65021e3fd
Tyler Barrios <tylerbarrios@gmail.com>
2024-10-26 19:39:32 -0400

    added book database and cover art

--- server/resolvers/bookResolvers.js
+++ server/resolvers/bookResolvers.js
@@ -1,28 +1,9 @@
 const Book = require('../models/Book');
-// const axios = require('axios');
 
 const resolvers = {
   Query: {
     books: async () => await Book.find(),
     book: async (_, { id }) => await Book.findById(id),
-
-    externalBooks: async () => {
-      try {
-        const response = await axios.get('http://books.cloudfoundry.com/data/books/');
-        return response.data.map((book) => ({
-          id: book.id,
-          title: book.title,
-          author: book.author,
-          description: book.description,
-          price: book.price || 0.0,
-          stock: book.stock || 0,
-          image: book.image_url
-        }));
-      } catch (error) {
-        console.error('Error fetching external books:', error);
-        throw new Error('Unable to fetch books from external API');
-      }
-    }
   },
 
   Mutation: {
@@ -39,6 +20,4 @@ const resolvers = {
   }
 };
 
-
 module.exports = resolvers;
-
